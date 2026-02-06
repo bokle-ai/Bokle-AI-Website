362502a649f5bb9d95076f81b2bc99ec114d6525
Seshadri Veerarghavan <seshadrivv28@gmail.com>
2026-02-06 00:29:03 -0500

    Add new allowed origin for CORS middleware

--- backend/server.py
+++ backend/server.py
@@ -24,6 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://bokle.in",
+        "https://bokle.in/admin",
         "https://www.bokle.in",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
